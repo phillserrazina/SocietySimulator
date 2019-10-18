a6c22c2c02fdf4850841d23815ae78091c4c33a6
phillserrazina <fserrazina99@gmail.com>
2019-10-18 11:49:25 +0100

    Added Random AI Agent Spawner

---
+++ SocietySim/Assets/_Scripts/Entities/AI/AISpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AISpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject agentPrefab = null;
+    [SerializeField] private Transform agentsParent = null;
+    private Mesh navMesh;
+
+    private const int NUM_OF_CITIZENS = 50;
+
+    private void Start() {
+        NavMeshTriangulation triangulatedNavMesh = NavMesh.CalculateTriangulation();
+        navMesh = new Mesh();
+        navMesh.vertices = triangulatedNavMesh.vertices;
+
+        for (int i = 0; i < NUM_OF_CITIZENS; i++) {
+            GameObject go = Instantiate(agentPrefab, GetRandomPoint(), Quaternion.identity);
+            go.transform.parent = agentsParent;
+        }
+    }
+
+    private Vector3 GetRandomPoint() {
+        Vector3 p1 = navMesh.vertices[Random.Range(0, navMesh.vertexCount)];
+        Vector3 p2 = navMesh.vertices[Random.Range(0, navMesh.vertexCount)];
+
+        Vector3 ans = Vector3.Lerp(p1, p2, Random.value);
+        ans.y = 1;
+
+        return ans;
+    }
+}
---
+++ SocietySim/Assets/_Scripts/Entities/AI/AppearanceGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class AppearanceGenerator : MonoBehaviour
+{
+    private void Start() {
+        var mat = GetComponent<MeshRenderer>().materials[0];
+        mat.color = new Color(Random.value, Random.value, Random.value);
+    }
+}
