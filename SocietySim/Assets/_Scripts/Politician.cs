10a1a3902685fbf758408e7c8940aeb79cda8708
phillserrazina <fserrazina99@gmail.com>
2019-10-23 20:31:39 +0100

    Added a description to the politicians

--- SocietySim/Assets/_Scripts/Politician.cs
+++ SocietySim/Assets/_Scripts/Politician.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Politician : MonoBehaviour
 {
     private string pName;
     private PoliticianEvent pEvent = new PoliticianEvent();
+    [SerializeField] private Text politicianInfo = null;
 
     private void Start() {
-        Debug.Log("I will " + pEvent.eventTypeInfo + " by " + pEvent.eventStrength + "% of " +  pEvent.targetInfo.ToLower() + " people!");
+        politicianInfo.text = string.Format("{0} to {1} of all {2} people!", pEvent.eventStrength, pEvent.eventTypeInfo, pEvent.targetInfo);
     }
 }
